009a4e56d70413bf854d5da023cbe480acbadf73
Breeshty <nafisatasnim97@gmail.com>
2025-12-23 18:49:04 +0600

    Add seller reporting system and admin reports tab

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use('/api/notifications', require('./routes/notificationRoutes'));
 app.use('/api/portfolios', require('./routes/portfolioRoutes'));
 app.use('/api/custom-orders', require('./routes/customOrderRoutes'));
 app.use('/api/wishlist', require('./routes/wishlistRoutes'));
-
+app.use('/api/reports', require('./routes/reportRoutes'));
 // Database Connection
 const connectDB = async () => {
   try {
