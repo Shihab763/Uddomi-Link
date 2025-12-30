4f0bc5ebfcd4b153f1de3288dddf32a90dee036e
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2025-12-31 03:58:03 +0600

    Update wishlistRoutes.js

--- server/routes/wishlistRoutes.js
+++ server/routes/wishlistRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {
+    getMyWishlist,
     addToWishlist,
     removeFromWishlist,
-    getMyWishlist,
-    checkInWishlist,
-    clearWishlist
+    createWishlistCategory,
+    addItemToCategory
 } = require('../controllers/wishlistController');
 const { protect } = require('../middleware/authMiddleware');
 
+router.get('/', protect, getMyWishlist);
 router.post('/add', protect, addToWishlist);
-router.post('/remove', protect, removeFromWishlist);
-router.get('/my', protect, getMyWishlist);
-router.get('/check/:itemType/:itemId', protect, checkInWishlist);
-router.delete('/clear', protect, clearWishlist);
+router.delete('/remove/:itemType/:itemId', protect, removeFromWishlist);
+router.post('/categories', protect, createWishlistCategory);
+router.post('/categories/add-item', protect, addItemToCategory);
 
 module.exports = router;
