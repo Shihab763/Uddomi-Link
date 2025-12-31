f9d5240599a3a703307a0dbb07cb49b303cf3a17
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2025-12-31 12:17:46 +0600

    Update portfolioRoutes.js

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {
-    createPortfolio,
-    getPortfolioItems,
-    getPortfolioById,
-    updatePortfolio,
-    deletePortfolio,
-    getMyPortfolios,
-    incrementFavoriteCount
-} = require('../controllers/portfolioController');
 const { protect } = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware');
 
-router.get('/', getPortfolioItems);
+const {
+  createPortfolio,
+  getMyPortfolios,
+  getPortfolioById,
+} = require('../controllers/portfolioController');
+
+router.post('/', protect, upload.single('media'), createPortfolio);
+router.get('/my', protect, getMyPortfolios);
 router.get('/:id', getPortfolioById);
-router.post('/', protect, createPortfolio);
-router.put('/:id', protect, updatePortfolio);
-router.delete('/:id', protect, deletePortfolio);
-router.get('/creator/my-portfolios', protect, getMyPortfolios);
-router.post('/:id/favorite', protect, incrementFavoriteCount);
 
 module.exports = router;
