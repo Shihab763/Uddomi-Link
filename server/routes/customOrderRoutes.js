d48be96117cda997867f73dc7f4033ba7056f5e1
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2025-12-31 12:01:37 +0600

    Update customOrderRoutes.js

--- server/routes/customOrderRoutes.js
+++ server/routes/customOrderRoutes.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {
-    createCustomOrder,
-    getMyCustomOrders,
-    getCustomOrderById,
-    updateCustomOrderStatus,
-    addMessageToOrder,
-    rateCustomOrder
-} = require('../controllers/customOrderController');
 const { protect } = require('../middleware/authMiddleware');
+const {
+  createCustomOrder,
+  getMyCustomOrders,
+  getCreatorCustomOrders,
+  updateCustomOrderStatus,
+} = require('../controllers/customOrderController');
 
 router.post('/', protect, createCustomOrder);
-router.get('/my-orders', protect, getMyCustomOrders);
-router.get('/:id', protect, getCustomOrderById);
-router.put('/:id/status', protect, updateCustomOrderStatus);
-router.post('/:id/message', protect, addMessageToOrder);
-router.post('/:id/rate', protect, rateCustomOrder);
+router.get('/my', protect, getMyCustomOrders);
+router.get('/creator', protect, getCreatorCustomOrders);
+router.patch('/:id', protect, updateCustomOrderStatus);
 
 module.exports = router;
