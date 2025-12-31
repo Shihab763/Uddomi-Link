390e45d2669da14f4a34d97998f3a1640adc6301
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2025-12-31 11:54:23 +0600

    Update searchRoutes.js

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {
-    searchItems,
-    getSearchSuggestions,
-    getSearchFilters,
-    getPopularSearches
-} = require('../controllers/searchController');
-const { protect, optionalAuth } = require('../middleware/authMiddleware');
+const { search } = require('../controllers/searchController');
 
-router.get('/', optionalAuth, searchItems);
-router.get('/suggestions', optionalAuth, getSearchSuggestions);
-router.get('/filters', optionalAuth, getSearchFilters);
-router.get('/popular', optionalAuth, getPopularSearches);
+router.get('/', search);
 
 module.exports = router;
