422fc50790f25cc21029593c841d111f340e1118
TauhidulHaq <tauhidul.sowrove43@gmail.com>
2026-01-02 23:30:04 +0600

    Update notificationSystemRoutes.js

--- server/routes/notificationSystemRoutes.js
+++ server/routes/notificationSystemRoutes.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { 
-    getNotifications, 
-    markAsRead, 
-    markAllAsRead,
-    getUnreadCount 
-} = require('../controllers/notificationSystemController'); // Importing from the NEW controller name
+const { protect } = require('../middleware/authMiddleware');
+const { getMyNotifications, markAsRead, markAllAsRead } = require('../controllers/notificationSystemController');
 
-const { createNotification } = require('../utils/notificationSystemHelper'); // Importing from the NEW helper name
-const { protect } = require('../middleware/authMiddleware'); 
-
-router.use(protect);
-
-
-router.get('/', getNotifications);
-router.get('/unread-count', getUnreadCount);
-router.put('/:id/read', markAsRead);
-router.put('/read-all', markAllAsRead);
+router.get('/', protect, getMyNotifications);
+router.put('/read-all', protect, markAllAsRead);
+router.put('/:id/read', protect, markAsRead);
 
 module.exports = router;
