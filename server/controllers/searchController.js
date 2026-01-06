3cd6697fd4b1b1de229e819162a698e4ec525cb1
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-01-06 22:00:35 +0600

    Update searchController.js

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,23 +1,21 @@
 const Product = require('../models/productModel');
 const User = require('../models/userModel');
 
-// @desc    Get search suggestions (Typeahead)
-// @route   GET /api/search/suggestions?q=keyword
-// @access  Public
+
 const getSuggestions = async (req, res) => {
   try {
     const query = req.query.q;
     if (!query) return res.json([]);
 
-    // Regex for case-insensitive partial match
+    
     const regex = new RegExp(query, 'i');
 
-    // 1. Find matching Products (limit 5)
+
     const products = await Product.find({ name: { $regex: regex } })
-      .select('name image price category')
+      .select('name imageUrl price category') 
       .limit(5);
 
-    // 2. Find matching Creators (limit 3)
+
     const creators = await User.find({
       name: { $regex: regex },
       roles: 'business-owner' 
@@ -29,38 +27,33 @@ const getSuggestions = async (req, res) => {
   }
 };
 
-// @desc    Advanced Search with Filters
-// @route   GET /api/search?q=...&type=...&min=...&max=...&sort=...
-// @access  Public
+
 const searchAll = async (req, res) => {
   try {
     const { q, type, min, max, sort } = req.query;
     const regex = q ? new RegExp(q, 'i') : null;
     let results = {};
 
-    // --- SEARCH PRODUCTS ---
+
     if (!type || type === 'products') {
       let queryObj = {};
       if (regex) queryObj.name = { $regex: regex };
 
-      // Price Filter
       if (min || max) {
         queryObj.price = {};
         if (min) queryObj.price.$gte = Number(min);
         if (max) queryObj.price.$lte = Number(max);
       }
 
-      // Sorting Logic
       let sortObj = {};
-      if (sort === 'highToLow') sortObj.price = -1; // Descending
-      if (sort === 'lowToHigh') sortObj.price = 1;  // Ascending
+      if (sort === 'highToLow') sortObj.price = -1; 
+      if (sort === 'lowToHigh') sortObj.price = 1;  
       
       const products = await Product.find(queryObj).sort(sortObj);
       results.products = products;
     }
 
-    // --- SEARCH CREATORS ---
-    // (Only if type is NOT 'products', i.e., 'creators' or 'all')
+
     if (!type || type === 'creators') {
       let userQuery = { roles: 'business-owner' };
       if (regex) userQuery.name = { $regex: regex };
