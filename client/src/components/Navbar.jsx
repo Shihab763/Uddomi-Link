6dbc362121999cb5165b5624a13aa38baea3f55d
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-01-06 21:02:05 +0600

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -63,7 +63,7 @@ function Navbar() {
               type="text"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              placeholder=" Search products, creators, skills..."
+              placeholder=" Search products and creators"
               className="w-full p-3 pl-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary bg-white text-dark"
             />
             <button 
