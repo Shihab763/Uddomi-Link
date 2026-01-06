f3b14f0ed09ee886b20206d64c51361c65dd9e0d
Shihab763 <shihab.un.sakibkhan@g.bracu.ac.bd>
2026-01-06 23:02:37 +0600

    Update wishlistModel.js

--- server/models/wishlistModel.js
+++ server/models/wishlistModel.js
@@ -6,31 +6,25 @@ const wishlistSchema = mongoose.Schema({
         ref: 'User',
         required: true
     },
-    items: [{
-        itemType: {
-            type: String,
-            enum: ['portfolio', 'product', 'creator', 'training'],
-            required: true
-        },
-        itemId: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            refPath: 'items.itemType'
-        },
-        addedAt: {
-            type: Date,
-            default: Date.now
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            savedQuantity: {
+                type: Number,
+                default: 1
+            },
+            addedAt: {
+                type: Date,
+                default: Date.now
+            }
         }
-    }],
-    categories: [{
-        name: String,
-        itemIds: [mongoose.Schema.Types.ObjectId]
-    }]
+    ]
 }, {
     timestamps: true
 });
-
-wishlistSchema.index({ user: 1 });
-wishlistSchema.index({ 'items.itemId': 1, 'items.itemType': 1 });
 
 module.exports = mongoose.model('Wishlist', wishlistSchema);
